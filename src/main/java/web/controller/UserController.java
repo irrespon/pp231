5305ff6c46cb4588b693d8149edcd49bb1069e57
Alexey Korneev <korneevkot@yandex.ru>
2022-02-22 00:27:59 +0300

    correct thymeleaf and UserController for edit user

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import web.model.User;
 import web.service.UserService;
 import web.service.UserServiceImpl;
@@ -17,12 +18,16 @@ public class UserController {
     @GetMapping("/")
     public String allUsers(Model model) {
         List<User> users = userService.allUsers();
-        model.addAttribute("users",users);
+        model.addAttribute("users", users);
         return "users";
     }
 
-    @GetMapping("/edit")
-    public String editPage() {
+    @GetMapping("/edit/{id}")
+    public String editPage(@PathVariable("id") int id, Model model) {
+
+        User user = userService.getById(id);
+        model.addAttribute("user", user);
         return "editPage";
     }
+
 }
